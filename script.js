c2255c23e8340b83a1c13fc1bcdfbe297f9edace
Ivan-Kukshyn <ivan.kukshyn@gmail.com>
2023-09-19 09:51:07 +0200

    Created a default grid sized 16x16 in script.js file and showed it with style.css file

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.getElementById("container");
+
+// Creates a default grid sized 16x16
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "gridItem";
+  };
+};
+
+makeRows(16, 16);
+
